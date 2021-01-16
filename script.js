ad384a9a3cc008f3a0810d27690e409bf31ef6c3
Nibir <mz.nibir@gmail.com>
2021-01-16 22:40:19 +0600

    email check

--- script.js
+++ script.js
@@ -3,3 +3,41 @@ let email = document.querySelector("#email");
 let phone = document.querySelector("#phone");
 let postal = document.querySelector("#postal");
 
+//define EventListener
+email.addEventListener("click",(e) => {
+    let email_re = /^([a-zA-Z0-9].?)+[^.]@([a-zA-Z0-9].?)+[^.]$/ ;
+    choice = prompt("Enter Your Email: ");
+
+
+let post = choice.match(email_re);
+
+    var text;
+if(post){
+    text = "Email is Valid!";
+    
+}else{
+    text = "Email is not Valid!";
+    
+}
+
+alert(text);
+
+    e.preventDefault();
+
+
+});
+
+phone.addEventListener("click",(e) => {
+    console.log("phone click");
+
+
+});
+
+
+postal.addEventListener("click",(e) => {
+    console.log("postal click");
+
+
+});
+
+
